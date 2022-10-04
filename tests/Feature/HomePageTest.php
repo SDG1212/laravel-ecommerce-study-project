143cbedab0373a0e6b33ccf018016b4aec6a3576
SDG1212 <sokol1294@gmail.com>
2022-10-05 02:36:33 +0300

    Redevelop Welcome Page to Home Page

---
+++ tests/Feature/HomePageTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomePageTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function test_home_page_is_rendered()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
