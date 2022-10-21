0bc372026af0099ce3f2d1acbe6c4753eede7f09
SDG1212 <sokol1294@gmail.com>
2022-10-21 19:09:22 +0300

    Transfer Home Page View Render from Controller to Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\SearchController;
 use App\View\Components\Newsletter;
@@ -17,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::view('/', 'home')->name('home');
 
 Route::post('/subscribe', [Newsletter::class, 'subscribe'])->name('newsletter.subscribe');
 
