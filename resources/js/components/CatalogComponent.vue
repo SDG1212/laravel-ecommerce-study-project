a28052a9ac1d7909173b29bc8c4dd270782465bb
SDG1212 <sokol1294@gmail.com>
2022-10-21 19:19:24 +0300

    Improve Show More Button Load Animation

--- resources/js/components/CatalogComponent.vue
+++ resources/js/components/CatalogComponent.vue
@@ -157,4 +157,54 @@
 
     color: #fff;
   }
+
+  .catalog__toggler {
+    position: relative;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    max-width: 256px;
+    padding: 12px 24px;
+    background: #c70101;
+    border-radius: 4px;
+    text-transform: uppercase;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 1;
+    color: #fff;
+    transition: 0.2s;
+  }
+
+  .catalog__toggler.--active {
+    background: #940101;
+    color: transparent;
+    pointer-events: none;
+  }
+
+  .catalog__toggler.--active:before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin: -12px;
+    display: block;
+    width: 24px;
+    height: 24px;
+    border: 2px solid #fff;
+    border-radius: 50%;
+    border-top-color: #940101;
+    animation: spin 1s ease-in-out infinite;
+    z-index: 1;
+  }
+
+  .catalog__toggler:hover {
+    background: #940101;
+  }
+
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
 </style>
