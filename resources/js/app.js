946c1bd197aa5083082283c9ec4459ff9896dcf0
SDG1212 <sokol1294@gmail.com>
2022-10-25 19:19:24 +0300

    Add Vue I18n Plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 import { clickOutside } from '@/directives/ClickOutsideDirective'
 import Search from '@/components/header/SearchComponent.vue'
 import Cart from '@/components/header/CartComponent.vue'
@@ -9,9 +10,12 @@ import Catalog from '@/components/catalog/CatalogComponent.vue'
 
 const pinia = createPinia().use(() => { window.alertBox })
 
+const i18n = createI18n({})
+
 const app = createApp({})
   .directive('click-outside', clickOutside)
   .use(pinia)
+  .use(i18n)
   .component('Cart', Cart)
   .component('Search', Search)
   .component('Catalog', Catalog)
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import vueI18n from '@intlify/vite-plugin-vue-i18n';
 
 export default defineConfig({
     plugins: [
@@ -23,6 +24,7 @@ export default defineConfig({
                 },
             },
         }),
+        vueI18n({}),
     ],
     resolve: {
         alias: {
