a9eda575b7764e26975710c0478de0815d6a2d73
SDG1212 <sokol1294@gmail.com>
2022-10-26 19:22:03 +0300

    Improve Product Card View

--- resources/js/components/catalog/ProductCardComponent.vue
+++ resources/js/components/catalog/ProductCardComponent.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="product-card">
-        <img :src="image" :alt="name" />
-        <button class="" @click="addProduct(id)">{{ name }}</button>
+      <a class="product-card__image-link" href="#">
+        <img class="product-card__image" :src="image" :alt="name" />
+      </a>
+      <div class="product-card__content">
+        <a class="product-card__name" href="#">{{ name }}</a>
+      </div>
+      <button class="product-card__add-to-cart" @click="addProduct(id)">{{ $t('add_to_cart') }}</button>
     </div>
 </template>
 
@@ -17,8 +22,70 @@
   }
 </script>
 
-<style>
+<style lang="scss">
   .product-card {
+    display: flex;
+    flex-direction: column;
   	width: 100%;
+
+    &__image {
+      max-width: 100%;
+      height: auto;
+      transition: 0.2s;
+    }
+
+    &__image-link:hover &__image {
+      opacity: 0.6;
+    }
+
+    &__content {
+      flex: 1 0 auto;
+      margin: 4px 0 8px;
+    }
+
+    &__name {
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      font-size: 14px;
+      font-weight: 600;
+      line-height: 1.3;
+      color: #333;
+      transition: 0.2s;
+
+      &:hover {
+        color: #940101;
+      }
+    }
+
+    &__add-to-cart {
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      padding: 8px 16px;
+      background: #c70101;
+      border-radius: 4px;
+      text-transform: uppercase;
+      font-size: 15px;
+      font-weight: 500;
+      line-height: 1;
+      color: #fff;
+      transition: 0.2s;
+
+      &:hover {
+        background: #940101;
+      }
+    }
   }
 </style>
+
+<i18n>
+{
+  ru: {
+    add_to_cart: 'Добавить в корзину',
+  },
+}
+</i18n>
