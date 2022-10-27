ac8a00ecd0b70c638521eaf5261fe13cd71536ea
SDG1212 <sokol1294@gmail.com>
2022-10-27 19:40:23 +0300

    Remove Unused Dependency in Product Service

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -5,7 +5,6 @@ namespace App\Services;
 use App\Repositories\IProductRepository;
 use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class CartService
