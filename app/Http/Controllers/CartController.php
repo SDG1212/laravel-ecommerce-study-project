99689ef36657c5cd2782d873dfce08f798b28936
SDG1212 <sokol1294@gmail.com>
2022-11-01 15:52:01 +0200

    Add Cart Edit Product Quantity Validation by Stock Quantity

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\CartService;
 use App\Http\Resources\CartCollection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class CartController extends Controller
 {
@@ -50,10 +52,21 @@ class CartController extends Controller
 	 */
 	public function editProduct(Request $request)
 	{
-		$validated_data = $request->validate([
+		$validated_data = Validator::make($request->all(), [
 			'id' => ['required', 'integer', 'exists:products'],
-			'quantity' => ['required', 'integer', 'min:1'],
-		]);
+			'quantity' => [
+				'required',
+				'integer',
+				'min:1',
+				function($attribute, $value, $fail) use ($request) {
+					$is_product_available = DB::table('products')->where('quantity', '>=', $value)->find($request->input('id'));
+
+					if (!$is_product_available) {
+						$fail('The ' . $attribute . ' is invalid.');
+					}
+				},
+			]
+		])->validated();
 
 		$products = $this->cartService->editProduct($validated_data['id'], $validated_data['quantity']);
 
