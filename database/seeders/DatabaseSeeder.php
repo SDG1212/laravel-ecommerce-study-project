0ac11539c825677cb3a6a275bb723459da5772df
SDG1212 <sokol1294@gmail.com>
2022-11-04 00:57:57 +0200

    Improve DB Seeder for Categories & Products Tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,31 +26,30 @@ class DatabaseSeeder extends Seeder
 
         $faker = Faker::create();
 
-        $products = [];
-
-        for ($i = 0; $i < 240; $i++) {
-            $products[] = [
-                'name' => $faker->words(5, true),
-                'image' => $faker->imageUrl(640, 480),
-                'price' => $faker->randomFloat(2, 1, 1000000),
-                'quantity' => $faker->randomNumber(2, false),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ];
-        }
-
-        DB::table('products')->insert($products);
-
-        $categories = [];
-
-        for ($i = 0; $i < 12; $i++) {
-            $categories[] = [
+        for ($i = 0; $i < 8; $i++) {
+            $category_id = DB::table('categories')->insertGetId([
                 'name' => $faker->words(5, true),
                 'created_at' => now(),
                 'updated_at' => now(),
-            ];
-        }
+            ]);
 
-        DB::table('categories')->insert($categories);
+            for ($y = 0; $y < 24; $y++) {
+                $products = [
+                    'name' => $faker->words(5, true),
+                    'image' => $faker->imageUrl(640, 480),
+                    'price' => $faker->randomFloat(2, 1, 1000000),
+                    'quantity' => $faker->randomNumber(2, false),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+
+                $product_id = DB::table('products')->insertGetId($products);
+
+                DB::table('category_has_products')->insert([
+                    'category_id' => $category_id,
+                    'product_id' => $product_id,
+                ]);
+            }
+        }
     }
 }
